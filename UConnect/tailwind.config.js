6c0bba26ba4a701abc88236f91be08dc93ba6a86
Joseph Tandyo <josephtandyo@gmail.com>
2024-11-17 22:45:50 -0700

    Started replacing colors, and added toggle to light mode

--- UConnect/tailwind.config.js
+++ UConnect/tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        uConnectDark: {
+          layer1: "#000000",
+          layer2Primary: "#414040",
+          layer2Secondary: "#1D1C1C",
+          layer3: "#C6C3C3",
+          textMain: "#FFFFFF",
+          textSub: "#C6C3C3",
+          accent: "#FC9D04",
+        },
+        uConnectLight: {
+          layer1: "#FFFFFF",
+          layer2Primary: "#F3F4F6",
+          layer2Secondary: "#E5E7EB",
+          layer3: "#D1D5DB",
+          textMain: "#000000",
+          textSub: "#6B7280",
+          accent: "#FC9D04",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
