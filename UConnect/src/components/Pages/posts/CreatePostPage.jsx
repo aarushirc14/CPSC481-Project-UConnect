42bbe74fd179291640c81da5114f8510d9f530f7
Darcy Bartolome <bartolomedarcy@gmail.com>
2024-11-20 17:39:06 -0700

    Added styles and code for TextEditor in CreatePost

--- UConnect/src/components/Pages/posts/CreatePostPage.jsx
+++ UConnect/src/components/Pages/posts/CreatePostPage.jsx
@@ -2,11 +2,32 @@
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import TipTap from "../../TipTap";
+
+import { IoMdSend } from "react-icons/io";
+
 
 export default function CreatePostPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-uConnectLight-background dark:bg-uConnectDark-background text-uConnectLight-textMain dark:text-uConnectDark-textMain text-center">
-      <h1> Create Post Page. Needs to be implemented </h1>
-    </div>
-  );
-}
+  const navigate = useNavigate();
+
+  const handlePostClick = () => {
+    // Add code here where it collects input text data 
+    navigate("/home");      // After posting either return to home or go straight to Post view, for now it's home
+  };
+  
+    return (
+      <div className="TextEditor">
+        <TipTap/>
+        <button
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent the click from propagating to the parent Link
+              handlePostClick();
+            }}
+            className="mt-5 px-10 py-2 border border-[#FC9D04] text-uConnectLight-textMain dark:text-uConnectDark-textMain rounded-lg hover:bg-[#FC9D04] hover:text-black"
+          >
+            <IoMdSend /> Post
+          </button>
+      </div>
+    );
+  }
+  
---
+++ UConnect/src/components/TipTap.jsx
@@ -0,0 +1,291 @@
+// The text Editor for 
+
+import { Color } from '@tiptap/extension-color'
+import ListItem from '@tiptap/extension-list-item'
+import TextStyle from '@tiptap/extension-text-style'
+import { EditorProvider, useCurrentEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Underline from '@tiptap/extension-underline'
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
+import Placeholder from '@tiptap/extension-placeholder'
+import React, { useCallback } from 'react'
+import { RiBold,
+        RiItalic,
+        RiStrikethrough,
+        RiUnderline,
+        RiFormatClear,
+        RiFontColor,
+        } from "react-icons/ri";
+import { MdOutlineUndo,
+        MdOutlineRedo,
+        MdHorizontalRule,
+        } from "react-icons/md";
+import { BsListOl,
+        BsListUl,
+ } from "react-icons/bs";
+import { IoImagesSharp } from "react-icons/io5";
+import { IoMdLink } from "react-icons/io";
+
+
+
+
+const MenuBar = () => {
+  const { editor } = useCurrentEditor()
+
+  if (!editor) {
+    return null
+  }
+
+  const addImage = useCallback(() => {
+    const url = window.prompt('URL')
+
+    if (url) {
+      editor.chain().focus().setImage({ src: url }).run()
+    }
+  }, [editor])
+
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink()
+        .run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+      .run()
+  }, [editor])
+
+  if (!editor) {
+    return null
+  }
+
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="control-group">
+      <div className="button-group">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleBold()
+              .run()
+          }
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          <RiBold />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleItalic()
+              .run()
+          }
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          <RiItalic />
+        </button>
+        <button
+            onClick={() => editor.chain().focus().toggleUnderline().run()}
+            className={editor.isActive('underline') ? 'is-active' : ''}
+        >
+            <RiUnderline />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .toggleStrike()
+              .run()
+          }
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        >
+          <RiStrikethrough />
+        </button>
+        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+          <RiFormatClear />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive('bulletList') ? 'is-active' : ''}
+        >
+          <BsListUl />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        >
+          <BsListOl />
+        </button>
+        <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+          <MdHorizontalRule />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .undo()
+              .run()
+          }
+        >
+          <MdOutlineUndo />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={
+            !editor.can()
+              .chain()
+              .focus()
+              .redo()
+              .run()
+          }
+        >
+          <MdOutlineRedo />
+        </button>
+        <input
+            type="color"
+            onInput={event => editor.chain().focus().setColor(event.target.value).run()}
+            value={editor.getAttributes('textStyle').color}
+            data-testid="setColor"
+          />
+        <button
+          onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+          className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+        >
+          <RiFontColor />
+        </button>
+        <button onClick={addImage}>
+            <IoImagesSharp />
+        </button>
+        <button onClick={setLink} className={editor.isActive('link') ? 'is-active' : ''}>
+           <IoMdLink/>
+          </button>
+      </div>
+    </div>
+  )
+}
+
+const extensions = [
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  Underline.configure({ types: [ListItem.name] }),
+  TextStyle.configure({ types: [ListItem.name] }),
+  Image.configure({inline: true,}),
+  Placeholder.configure({
+    // Use a placeholder:
+    placeholder: 'Write the text here...',
+    // Use different placeholders depending on the node type:
+    // placeholder: ({ node }) => {
+    //   if (node.type.name === 'heading') {
+    //     return 'What’s the title?'
+    //   }
+
+    //   return 'Can you add some further context?'
+    // },
+  }),
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: true, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: true, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+  Link.configure({
+    openOnClick: true,
+    autolink: true,
+    linkOnPaste: true,
+    defaultProtocol: 'https',
+    protocols: ['http', 'https'],
+    isAllowedUri: (url, ctx) => {
+      try {
+        // construct URL
+        const parsedUrl = url.includes(':') ? new URL(url) : new URL(`${ctx.defaultProtocol}://${url}`)
+
+        // use default validation
+        if (!ctx.defaultValidate(parsedUrl.href)) {
+          return false
+        }
+
+        // disallowed protocols
+        const disallowedProtocols = ['ftp', 'file', 'mailto']
+        const protocol = parsedUrl.protocol.replace(':', '')
+
+        if (disallowedProtocols.includes(protocol)) {
+          return false
+        }
+
+        // only allow protocols specified in ctx.protocols
+        const allowedProtocols = ctx.protocols.map(p => (typeof p === 'string' ? p : p.scheme))
+
+        if (!allowedProtocols.includes(protocol)) {
+          return false
+        }
+
+        // disallowed domains
+        const disallowedDomains = ['example-phishing.com', 'malicious-site.net']
+        const domain = parsedUrl.hostname
+
+        if (disallowedDomains.includes(domain)) {
+          return false
+        }
+
+        // all checks have passed
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+    shouldAutoLink: url => {
+      try {
+        // construct URL
+        const parsedUrl = url.includes(':') ? new URL(url) : new URL(`https://${url}`)
+
+        // only auto-link if the domain is not in the disallowed list
+        const disallowedDomains = ['example-no-autolink.com', 'another-no-autolink.com']
+        const domain = parsedUrl.hostname
+
+        return !disallowedDomains.includes(domain)
+      } catch (error) {
+        return false
+      }
+    },
+
+  }),
+]
+
+const content = ``
+
+
+
+export default function TipTap() {
+  return (
+    <div className = "text-editor">
+        <EditorProvider slotBefore={<MenuBar />} extensions={extensions} content={content}> </EditorProvider>
+    </div>
+  )
+}
