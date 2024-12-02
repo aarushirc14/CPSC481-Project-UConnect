0986ff18e2270a22df222ddb303b5c2d8dfdc668
aarushirc14 <AarushiRC14@gmail.com>
2024-12-02 11:51:54 -0700

    adding back swc to fix bug

--- UConnect/vite.config.js
+++ UConnect/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { ghPages } from "vite-plugin-gh-pages";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ghPages(),
-  ],
-  base: "/UConnect/", 
+  plugins: [react()],
+  base: "/UConnect/", // Replace "UConnect" with your GitHub repository name
 });
-
